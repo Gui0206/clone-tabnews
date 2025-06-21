a9420d66796cfe816e644ddf8f8ac7934dfa1026
Guilherme Holanda <142675963+Gui0206@users.noreply.github.com>
2025-06-21 09:46:23 -0300

    add `on construction` text

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <p>Não seguimos o hype</p>
+      <p>Pensamos por nós mesmos</p>
+      <p>Buscamos a verdade</p>
+      <p>Acreditamos na humildade</p>
+      <p>Acreditamos na matemática</p>
+      <p>Não temos espaço para bullshit</p>
+    </div>
+  );
 }
 
 export default Home;
