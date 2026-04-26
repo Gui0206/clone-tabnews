aebd87d44b2395991e4bb40921dd52a47655c5d6
Guilherme Holanda <142675963+Gui0206@users.noreply.github.com>
2026-04-25 21:12:57 -0300

    sql injection api

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,27 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "deu certo" });
+  const updatedAt = new Date().toISOString();
+
+  const dbVersionQuery = await database.query("SHOW server_version;");
+  const dbVersion = dbVersionQuery.rows[0].server_version
+
+  const maxConnectionsQuery = await database.query("SHOW max_connections;")
+  const maxConnections = maxConnectionsQuery.rows[0].max_connections
+  
+  const openConnectionsQuery = await database.query("SELECT count(*) FROM pg_stat_activity WHERE datname = 'local_db';")
+  const openConnections = openConnectionsQuery.rows[0].count
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+          version: dbVersion,
+          max_connections: parseInt(maxConnections),
+          openConnections: parseInt(openConnections),
+      }
+    }
+  });
 }
 
 export default status;
