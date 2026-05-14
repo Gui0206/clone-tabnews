9910d94ea6b66c91032aaa8459e0ee217d9f0481
Guilherme Holanda <142675963+Gui0206@users.noreply.github.com>
2026-05-14 12:09:34 -0300

    fix prettier style linting

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,25 +4,25 @@ async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
   const dbVersionQuery = await database.query("SHOW server_version;");
-  const dbVersion = dbVersionQuery.rows[0].server_version
+  const dbVersion = dbVersionQuery.rows[0].server_version;
 
-  const maxConnectionsQuery = await database.query("SHOW max_connections;")
-  const maxConnections = maxConnectionsQuery.rows[0].max_connections
-  
+  const maxConnectionsQuery = await database.query("SHOW max_connections;");
+  const maxConnections = maxConnectionsQuery.rows[0].max_connections;
+
   const openConnectionsQuery = await database.query(
-    "SELECT count(*) FROM pg_stat_activity WHERE datname = current_database(); "
-  )
-  const openConnections = openConnectionsQuery.rows[0].count
+    "SELECT count(*) FROM pg_stat_activity WHERE datname = current_database(); ",
+  );
+  const openConnections = openConnectionsQuery.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
-          version: dbVersion,
-          max_connections: parseInt(maxConnections),
-          openConnections: parseInt(openConnections),
-      }
-    }
+        version: dbVersion,
+        max_connections: parseInt(maxConnections),
+        openConnections: parseInt(openConnections),
+      },
+    },
   });
 }
 
